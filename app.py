f24a659d2332224fa99cbbc28a7291b185a0faa8
arborter <110191602+arborter@users.noreply.github.com>
2023-01-19 12:39:46 -0500

    Update

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, redirect, render_template
+from jinja2 import Template
+import sqlite3
+
+
+# Configure application
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-if __name__ == '__main__':
-    app.run()
+conn = sqlite3.connect('comm.db')
+cursor = conn.cursor()
+
+conn.commit()
+
+@app.route('/')
+def index():
+    comments = cursor.execute("SELECT * FROM comm")
+    return render_template("index.html", comments=comments)
